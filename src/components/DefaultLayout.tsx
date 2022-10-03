a3b2631b877e8c4bf7a89299674c85bd02c994fe
levijubb1 <levijubb@gmail.com>
2022-10-03 19:40:14 +1000

    Added background to login page.

--- src/components/DefaultLayout.tsx
+++ src/components/DefaultLayout.tsx
@@ -18,6 +18,10 @@ const DefaultLayout: React.FC<Props> = ({
 			<Head>
 				<title>{pageName}</title>
 				<meta name="description" content={pageDescription} />
+				<link
+					rel="icon"
+					href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👕</text></svg>"
+				/>
 			</Head>
 			<Header />
 			<div className="container mx-auto relative p-4">{children}</div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,11 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import { trpc } from '../utils/trpc';
-import Link from 'next/link';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 
 const Home: NextPage = () => {
 	const router = useRouter();
 	const { data: session } = useSession();
-	const hello = trpc.useQuery(['example.getAll']);
-	// const hello = trpc.useQuery(['example.hello', { text: 'from tRPC' }]);
 
 	if (session) {
 		router.push('/products');
@@ -20,21 +16,45 @@ const Home: NextPage = () => {
 			<Head>
 				<title>Product App</title>
 				<meta name="description" content="Tester app on the t3 stack" />
-				<link rel="icon" href="/favicon.ico" />
+				<link
+					rel="icon"
+					href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🔒</text></svg>"
+				/>
 			</Head>
 
-			<main className="container mx-auto flex flex-col items-center justify-center min-h-screen p-4">
-				<button className="btn btn-circle h-[250px] w-[250px]">Sign in</button>
-				<Link href="/products">
-					<button className="btn">Product</button>
-				</Link>
-				<Link href="/auth/sign-in">
-					<button className="btn">Sign In</button>
-				</Link>
-
-				<div className="pt-6 text-2xl text-blue-500 flex justify-center items-center w-full">
-					{hello.data ? <p>{hello.data[0]?.age}</p> : <p>Loading..</p>}
-				</div>
+			<main
+				style={{ backgroundImage: "url('/landing-bg.png')" }}
+				className="bg-no-repeat bg-cover mx-auto flex flex-col  items-center justify-center min-h-screen p-4"
+			>
+				<button onClick={() => router.push('/auth/sign-in')} className="btn absolute z-10 btn-circle h-[250px] w-[250px]">
+					Sign in
+					{/* <svg
+						className="absolute transform translate-x-[11px] "
+						viewBox="0 0 300 300"
+						style={{
+							height: '350px',
+							width: '350px'
+						}}
+					>
+						<path
+							id="curve"
+							fill="none"
+							stroke="none"
+							d="M 32.550491,148.48008 A -108.15144,-108.15144 0 0 1 140.70194,40.328644 -108.15144,-108.15144 0 0 1 248.85338,148.48008 -108.15144,-108.15144 0 0 1 140.70194,256.63153 -108.15144,-108.15144 0 0 1 32.550491,148.48008 Z"
+						/>
+						<text
+							fontSize="25"
+							fill="#ffffff"
+							letterSpacing="2"
+							fontFamily="sans-serif"
+							fontWeight="bold"
+						>
+							<textPath xlinkHref="#curve" startOffset="5">
+								A totally groovy product CRUD app == By LJ == 
+							</textPath>
+						</text>
+					</svg> */}
+				</button>
 			</main>
 		</>
 	);
