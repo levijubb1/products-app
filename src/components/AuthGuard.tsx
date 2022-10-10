05dd2793ae1e674db8a4cb8d5d05017d5b9c486b
levijubb <levijubb@gmail.com>
2022-10-10 13:29:14 +1000

    Rewrote dashboard authguard.

--- src/components/AuthGuard.tsx
+++ src/components/AuthGuard.tsx
@@ -1,30 +1,33 @@
-import { useSession } from 'next-auth/react';
-import Router from 'next/router';
-import { ReactNode } from 'react';
+import { signIn, useSession } from 'next-auth/react';
+import { useEffect, ReactNode } from 'react';
 import Loading from './Loading';
 
 type Props = {
 	children: ReactNode;
 };
 
+/**
+ * @link https://cloudcoders.xyz/blog/create-protected-routes-in-nextjs/
+ */
 const AuthGuard = ({ children }: Props) => {
 	const { data: session, status } = useSession();
+	const isUser = !!session?.user;
+	useEffect(() => {
+		if (status === 'loading') return;
+		if (!isUser) signIn();
+	}, [isUser, status]);
 
-	if (status === 'loading') {
-		return (
-			<div className="flex justify-center items-center h-[100vh]">
-				<Loading />
-			</div>
-		);
-	}
-
-	if (status === 'unauthenticated' && !session) {
-		Router.push(Router.basePath);
-	}
-
-	if (session) {
+	if (isUser) {
 		return children;
 	}
+
+	// Session is being fetched, or no user.
+	// If no user, useEffect() will redirect.
+	return (
+		<div className="flex justify-center items-center h-[100vh]">
+			<Loading />
+		</div>
+	);
 };
 
 export default AuthGuard;
